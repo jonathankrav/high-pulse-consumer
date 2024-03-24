249b092d24b7742569d60784a7c3c47d0caab59e
jonathankrav <jk.fcdk@gmail.com>
2024-03-25 01:11:44 +0200

    hw

---
+++ high-pulse-consumer/src/main/java/telran/java51/HighPulseConsumerApplication.java
@@ -0,0 +1,13 @@
+package telran.java51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighPulseConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighPulseConsumerApplication.class, args);
+	}
+
+}
